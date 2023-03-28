2d23b254d3035034b2aec25f6df4433501edfc71
Tatiana <Rybalkinatr@gmail.com>
2023-03-28 12:32:37 +0400

    Метод по выводу массива на экран

--- Task/Program.cs
+++ Task/Program.cs
@@ -17,3 +17,11 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
